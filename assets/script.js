d90e8b42111f449a4969e003ccd6a45ebc746e94
vkaszuba <codeword.vika@gmail.com>
2020-11-17 19:30:06 -0800

    Doc cleanup

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,11 @@
 
-
 // Button that creates a name for textbox
 const generateNameBtn = document.querySelector("#generateNameBtn");
 generateNameBtn.addEventListener("click", fetchNames);
 
-
-
 function fetchNames() {
 
   const APIkey = "vi257045419";
-
   let url = `https://www.behindthename.com/api/random.json?usage=ita&gender=f&key=${APIkey}`
 
   // Call the fetch function passing the url of the API as a parameter
