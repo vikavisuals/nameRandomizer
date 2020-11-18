35b3d957800d083455e3606bc69cadadf054123b
vkaszuba <codeword.vika@gmail.com>
2020-11-17 18:39:29 -0800

    Fetch works! Names display on textbox

--- assets/script.js
+++ assets/script.js
@@ -1,32 +1,30 @@
 
 
+function fetchNames() {
+
+  const APIkey = "vi257045419";
+
+  let url = `https://www.behindthename.com/api/random.json?usage=ita&gender=f&key=${APIkey}`
+
+  // Call the fetch function passing the url of the API as a parameter
+  fetch(url)
+    // Transform the data into json
+    .then(function (response) {
+      return response.json();
+    })
+    // Your code for handling the data you get from the API
+    .then(function (data) {
+      console.log(data);
+      console.log(data.names["0"]);
+      let generatedName = document.querySelector("#generatedName");
+      generatedName.value = data.names;
+    })
+
+};
+
+
+
 
 // Button that creates a name for textbox
 const generateNameBtn = document.querySelector("#generateNameBtn");
-generateNameBtn.addEventListener("click", createName);
-
-// Textbox that will show a name
-function createName() {
-  let generatedName = document.querySelector("#generatedName");
-  generatedName.value = url;
-
-}
-
-
-
-let url = "https://www.behindthename.com/api/random.json?usage=ita&gender=f&key=vi257045419"
-
-fetch(url) // Call the fetch function passing the url of the API as a parameter
-
-  .then((resp) => resp.json()) // Transform the data into json
-
-  .then(function (data) {
-    // Your code for handling the data you get from the API
-    console.log(data)
-    console.log(data.names)
-
-  })
-  .catch(function (error) {
-    // This is where you run code if the server returns any errors
-    console.log(error);
-  });
+generateNameBtn.addEventListener("click", fetchNames);
